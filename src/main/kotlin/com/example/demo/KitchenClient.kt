f1fd9edc690311695b481c541f33533bdd5d6579
Yoonjeong Kwon <devchloe.kwon@gmail.com>
2021-08-01 17:02:38 +0900

    feat: 주방 서비스를 외부 서비스로 분리

---
+++ src/main/kotlin/com/example/demo/KitchenClient.kt
@@ -0,0 +1,14 @@
+package com.example.demo
+
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Flux
+
+@Component
+class KitchenClient {
+    private val webClient: WebClient = WebClient.create("http://localhost:5000")
+
+    fun getDishes(): Flux<Dish> {
+        return webClient.get().uri("/success").retrieve().bodyToFlux(Dish::class.java)
+    }
+}
--- src/main/kotlin/com/example/demo/PoliteServer.kt
+++ src/main/kotlin/com/example/demo/PoliteServer.kt
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 
 @RestController
-class PoliteServer(private val kitchen: KitchenService) {
+class PoliteServer(private val kitchen: KitchenService, private val kitchenClient: KitchenClient) {
     @GetMapping()
     fun serveDishes(): Flux<Dish> {
         return kitchen.getDishes().map { it.deliver() }
+    }
+
+    @GetMapping("/remote")
+    fun serveDishesFromRemote(): Flux<Dish> {
+        return kitchenClient.getDishes().map { it.deliver() }
     }
 }
