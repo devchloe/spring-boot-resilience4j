656ad94a15fff7f815f1dd68ac700a2dcdd34da9
Yoonjeong Kwon <devchloe.kwon@gmail.com>
2021-08-01 17:51:38 +0900

    feat: @CircuitBreaker 적용

--- src/main/kotlin/com/example/demo/KitchenClient.kt
+++ src/main/kotlin/com/example/demo/KitchenClient.kt
@@ -1,13 +1,18 @@
 package com.example.demo
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Flux
+
+const val CIRCUIT_BREAKER_KITCHEN = "kitchen"
 
 @Component
 class KitchenClient {
     private val webClient: WebClient = WebClient.create("http://localhost:5000")
 
+
+    @CircuitBreaker(name = CIRCUIT_BREAKER_KITCHEN)
     fun getDishes(): Flux<Dish> {
         return webClient.get().uri("/").retrieve().bodyToFlux(Dish::class.java)
     }
